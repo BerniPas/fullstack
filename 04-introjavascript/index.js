3cce62fe69d2185890ce4a5dd7f1a9c3413cae1c
Berni <pbernal@mendoza.edu.ar>
2024-06-18 21:30:58 -0300

    Intro a JS

---
+++ 04-introjavascript/index.js
@@ -0,0 +1,40 @@
+
+
+//estos son los comentarios de una sóla líena en javascript
+
+/*
+Cometarios 
+de 
+varias 
+lineas
+en JS
+*/
+
+//let hola = 'pepe'
+
+/*
+let hola = 'pepe'
+let hola = 'pepe'
+let hola = 'pepe'
+let hola = 'pepe'
+let hola = 'pepe'
+let hola = 'pepe'
+let hola = 'pepe'
+let hola = 'pepe'
+let hola = 'pepe'
+*/
+
+//Variables: espacios de memoria para almacenar datos
+//1. con var
+// palabras reservadas: son utilizadas sólo para las definiciones del lenguaje de programación
+var nombre;
+var email
+var edad;
+var pais
+
+
+
+
+
+
+
