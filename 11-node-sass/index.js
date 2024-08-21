536cd3e55065ce7297d2218d83f67c925420a13f
Berni <pbernal@mendoza.edu.ar>
2024-08-20 21:30:15 -0300

    Intro a Node

---
+++ 11-node-sass/index.js
@@ -0,0 +1,17 @@
+
+// Estamos utilizando un módulo nativo de Node.js
+// Con los múdulos de Node, NO necesitamos instalar nada
+// os es el módulo de Node.js que nos permite trabajar con el sistema operativo
+
+// importamos el módulo os de Node.js
+const os = require('os');
+
+
+console.log(os.cpus());
+console.log(os.platform());
+console.log(os.version());
+
+
+
+
+
