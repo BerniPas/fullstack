01285312aea1bd8c69cdcf3f6d7531cb7603ebe4
Berni <pbernal@mendoza.edu.ar>
2024-09-10 21:30:19 -0300

    JS avanzado

--- 13-ejsass/cargarProductos.js
+++ 13-ejsass/cargarProductos.js
@@ -1,10 +1,76 @@
 
+/* 
+    1. Creamos las variables de nuestra app
+ */
 
 let saludo = document.getElementById("bienvenida");
 
+let botonCargar = document.getElementById("botonCargar");
+// variables del array de los productos
+let misProductos = [];  // array vacio
+
+// Creamos una función para cargar los productos
+
+botonCargar.addEventListener("click", (event) => {
+
+event.preventDefault();
+
+console.log(event);
+
+let formulario = document.getElementById("formulario");
+
+
+// DOM: Document Object Model = document 
+
+let nombreProducto = document.getElementById("nombreProducto").value;
+let precioProducto = document.getElementById("precioProducto").value;
+let stockProducto = document.getElementById("stockProducto").value;
+let descripcionProducto = document.getElementById("descripcionProducto").value;
+let imagenProducto = document.getElementById("imagenProducto").value;
+
+
+
+// 2. Creamos un objeto literal con los datos de un producto
+let producto = {
+    nombre: nombreProducto,
+    precio: precioProducto,
+    stock: stockProducto,
+    descripcion: descripcionProducto,
+    imagen: imagenProducto
+} 
+
+// 3. Agregamos el producto al array de productos
+misProductos.push(producto);
+
+console.log(misProductos);
+
+// 4. Limpiamos los campos del formulario
+limpiarCampos();
+
+// 4. Limpiamos los campos del formulario de otra forma
+//formulario.reset();
+
+// 5. Guardamos los productos en el local storage
+localStorage.setItem("productos", JSON.stringify(misProductos));
+
+});
+
+// función para limpiar los campos del formulario
+const limpiarCampos = () => {
+    nombreProducto.value = "";
+    precioProducto.value = "";
+    stockProducto.value = "";
+    descripcionProducto.value = "";
+    imagenProducto.value = "";
+}
+
+
+
+
+
 
 // imprimimos en la pantalla el saludo al cliente que se logueo
-// tomamos el dato desde el localstorage: memoria loacal del navegador
+// tomamos el dato desde el localstorage: memoria local del navegador
 
 
 saludo.innerHTML = `Bienvenido ${localStorage.getItem("email")}`;
@@ -17,3 +83,22 @@ salir.addEventListener("click", (e) => {
     localStorage.clear();
     window.location.href = "./login.html";
 });
+
+
+// 6. Vramos una función para cargar la tabla con los productos
+let botonListar = document.getElementById("botonListar");
+
+botonListar.addEventListener("click", (e) => {
+    e.preventDefault();
+    cargarTabla();
+});
+
+const cargarTabla = () => {
+
+    let bodyTabla = document.getElementById("bodyTabla");
+
+    bodyTabla.innerHTML = "";
+
+    let fila = document.createElement('tr');
+
+};
---
+++ 13-ejsass/data.js
@@ -0,0 +1,31 @@
+
+
+let productos = [
+
+    {
+        "id": 1,
+        "nombre": "Manzana Roja",
+        "precio": 0.99,
+        "stock": 10,
+        "descripcion": "Manzana roja, jugosa y dulce.",
+        "imagen": "https://picsum.photos/200/300"
+    },
+    {
+        "id": 2,
+        "nombre": "Banana",
+        "precio": 0.75,
+        "stock": 15,
+        "descripcion": "Manzana roja, jugosa y dulce.",
+        "imagen": "https://picsum.photos/200/300"
+    },
+    {
+        "id": 3,
+        "nombre": "Naranja",
+        "precio": 1.29,
+        "stock": 8,
+        "descripcion": "Manzana roja, jugosa y dulce.",
+        "imagen": "https://picsum.photos/200/300"
+    }
+
+];
+
